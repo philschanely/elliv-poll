d9fc74613a907ced1ac2a48c36be984663fd6e8f
Emily O'Grady <emilyjogrady@cedarville.edu>
2017-04-10 15:44:13 -0400

    Basic setup of files

---
+++ assets/vote.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.nominee-header').on('click', function() {
+        if ($(window).width() < 767) {
+                $(this).children('.dropdown-arrow').toggleClass('rotated');
+                $(this).next().slideToggle('slow', function() {
+                    // Animation complete.
+                });
+            } else {
+                console.log('More than 767');
+                $(this).next('.nominee-description').addClass('show');
+                $('.shade').addClass('show');
+                $('.close').on('click', function () {
+                    $('.nominee-description').removeClass('show');
+                    $('.shade').removeClass('show');
+                });
+            }
+        });
+    $('.nominee-vote').on('click', function() {
+        $(this).toggleClass('select-circle');
+        $(this).parent().toggleClass('select-nominee')
+    });
+});
